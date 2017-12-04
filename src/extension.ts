23b48f69223d5a61e954a5f8d02a3e057e92bbb0
Talv <krz.krawczuk@gmail.com>
2017-12-04 01:45:20 +0100

    introduce config options

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,11 @@
 import * as path from 'path';
 
-import { workspace, ExtensionContext } from 'vscode';
+import { workspace, ExtensionContext, window, Disposable } from 'vscode';
 import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient';
 
 export function activate(context: ExtensionContext) {
-    let serverModule = context.asAbsolutePath(path.join('out', 'src', 'server.js'));
-    let debugOptions = { execArgv: ['--nolazy', '--debug-brk=3456'] };
+    let serverModule = context.asAbsolutePath(path.join('node_modules', 'plaxtony', 'lib', 'service', 'lsp-run.js'));
+    let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
 
     let serverOptions: ServerOptions = {
         run : { module: serverModule, transport: TransportKind.ipc },
@@ -18,7 +18,7 @@ export function activate(context: ExtensionContext) {
     const clientOptions: LanguageClientOptions = {
         documentSelector: [{scheme: 'file', language: 'galaxy'}],
         synchronize: {
-            configurationSection: 'plaxtony',
+            configurationSection: 'sc2galaxy',
             // Notify the server about file changes in the workspace
             // fileEvents: workspace.createFileSystemWatcher('**/.galaxy')
         },
@@ -29,6 +29,18 @@ export function activate(context: ExtensionContext) {
 
     const client = new LanguageClient('plaxtony', 'Plaxtony Language Server', serverOptions, clientOptions);
     let disposable = client.start();
+
+    (async () => {
+        let statusHandle: Disposable;
+        await client.onReady();
+        client.onNotification('indexStart', (params: any) => {
+            statusHandle = window.setStatusBarMessage('$(search) Indexing SC2 archives..');
+        });
+        client.onNotification('indexEnd', (params: any) => {
+            statusHandle.dispose();
+            window.setStatusBarMessage('Indexing of SC2 archives completed!', 3000);
+        });
+    })();
 
     context.subscriptions.push(disposable);
 }
--- src/server.ts
+++
@@ -1,5 +0,0 @@
-import * as path from 'path';
-import { Server } from 'plaxtony/lib/service/server';
-
-const server = new Server();
-server.createConnection().listen();
