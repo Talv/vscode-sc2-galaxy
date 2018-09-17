2689a28441203ecc2ac4b0e03c4f02211b1321d3
Talv <krz.krawczuk@gmail.com>
2018-09-17 10:47:17 +0200

    command: s2galaxy.verifyScript

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 
-import { workspace, ExtensionContext, window, Disposable, languages, CompletionItemProvider, TextDocument, CompletionItem, CompletionItemKind, SnippetString, ProgressLocation, Progress } from 'vscode';
+import { workspace, ExtensionContext, window, Disposable, languages, CompletionItemProvider, TextDocument, CompletionItem, CompletionItemKind, SnippetString, ProgressLocation, Progress, commands } from 'vscode';
 import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind, Position, CancellationToken } from 'vscode-languageclient';
 
 interface ProgressProxy {
@@ -27,7 +27,7 @@ function createProgressNotification() {
 }
 
 export function activate(context: ExtensionContext) {
-    let serverModule = context.asAbsolutePath(path.join('node_modules', 'plaxtony', 'lib', 'service', 'lsp-run.js'));
+    let serverModule = context.asAbsolutePath(path.join('node_modules', 'plaxtony', 'lib', 'src', 'service', 'lsp-run.js'));
     let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
 
     let serverOptions: ServerOptions = {
@@ -68,6 +68,11 @@ export function activate(context: ExtensionContext) {
             window.setStatusBarMessage('Indexing of SC2 archives completed!', 2000);
         });
     })();
+
+    context.subscriptions.push(commands.registerTextEditorCommand('s2galaxy.verifyScript', async (textEditor, edit) => {
+        const r = await client.sendRequest('document/checkRecursively', {uri: textEditor.document.uri.toString()});
+        window.setStatusBarMessage(r === true ? `[[ ✓ ]] SUCCESS!` : `[[ x ]] FAILED!`, 1500);
+    }));
 
     context.subscriptions.push(disposable);
 }
